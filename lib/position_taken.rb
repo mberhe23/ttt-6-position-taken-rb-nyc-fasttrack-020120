1db9216ffec13beb60440700a43fdc3eea2908ff
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-04 02:29:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 
-# board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 # index = ["X","0"]
 
 def position_taken?(board, index)
